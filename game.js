6ddf7266590bbb20ac0afe4355aaabf2f3518b5f
scare-destiny <zhenya.venger@gmail.com>
2022-04-06 18:44:17 +0300

    Added UI to the game

--- game.js
+++ game.js
@@ -2,75 +2,126 @@
 
 const ITEMS = ["rock", "paper", "scissors"];
 const MAX_ROUNDS = 5;
-let playerScore = 0;
-let computerScore = 0;
-let gameDraw = 0;
+let gameScore = [0, 0, 0]
+let currentRound = 0;
 
 
-
-function game() {
-	for (let i = 0; i < MAX_ROUNDS; i++) {
-		const playerSelection = prompt("What do you choose?", "Don't want to play, just kill me")
-		const computerSelection = computerPlay();
-		playRound(playerSelection, computerSelection);
-	}
-	declareWinner();
-
-}
 
 function declareWinner() {
-	console.log(`Final score by player: ${playerScore}. Computer: ${computerScore} Tie: ${gameDraw}`);
-	playerScore > computerScore ? console.log('Player wins the game!')
-		: playerScore === computerScore ? console.log("Game Tie :(")
-		: console.log("Computer wins :(((");
+	gameScore[0] > gameScore[1] ? alert('Player wins the game!')
+		: gameScore[0] === gameScore[1] ? alert("Game Tie :(")
+			: alert("Computer wins :(((");
+	setTimeout(resetGlobals, 1000);
 }
+
+function resetGlobals() {
+	gameScore = [0, 0, 0];
+	currentRound = 0;
+	document.getElementById('results-container').style.display = 'none';
+
+};
 
 
 function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase();
-	console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
 
-	if (!validateSelection(playerSelection)) {
-		alert(`Item ${playerSelection} doesn't exist, generating random item for player`);
-		playerSelection = ITEMS[Math.floor(Math.random() * ITEMS.length)];
-	}
-
-	let playerVictory = `Yay, you won this round: ${playerSelection} beats ${computerSelection}`;
-	let computerVictory = `Phew, you lost, computer wins. 
-	    ${computerSelection} is superior over ${playerSelection}`;
-	// TODO return string declaring the winner
+	displaySelection(playerSelection, computerSelection);
 
 	if (playerSelection === "rock" && computerSelection === "scissors") {
-		console.log(playerVictory);
-		return playerScore += 1;
+		gameScore[0] += 1;
+		declareRoundWinner('player');
 	}
 	else if (playerSelection === "paper" && computerSelection === "rock") {
-		console.log(playerVictory);
-		return playerScore += 1;
+		gameScore[0] += 1;
+		declareRoundWinner('player');
 	}
 	else if (playerSelection === "scissors" && computerSelection === "paper") {
-		console.log(playerVictory);
-		return playerScore += 1;
+		gameScore[0] += 1;
+		declareRoundWinner('player');
 	}
 	else if (playerSelection === computerSelection) {
-		console.log(gameDraw);
-		return gameDraw += 1;
+		gameScore[2] += 1;
+		declareRoundWinner('draw');
 	}
 	else {
-		console.log(computerVictory);
-		return computerScore += 1;
+		gameScore[1] += 1;
+		declareRoundWinner('computer');
+	}
+	updateScore(gameScore);
+
+	currentRound += 1;
+	isFinalround(currentRound)
+
+
+}
+
+function isFinalround(round) {
+	if (round === MAX_ROUNDS) {
+		declareWinner();
+	}
+	return;
+}
+
+function displaySelection(firstSelection, secondSelection) {
+	let para = document.getElementById('chosen-items');
+	para.textContent = (`Player chose ${firstSelection} and computer picked ${secondSelection}`);
+
+}
+
+function declareRoundWinner(winner) {
+
+	let para = document.getElementById('running-score');
+	para.classList.remove('looser');
+	switch (winner) {
+		case 'player':
+			para.textContent = 'Player won';
+			break;
+		case 'computer':
+			para.classList.add('looser');
+			para.textContent = 'Computer won';
+			break;
+		case 'draw':
+			para.textContent = 'Draw, no one won';
+			break;
+	};
+}
+
+function updateScore(array) {
+	let rows = document.querySelectorAll('td');
+	for (i = 0; i < rows.length; i++) {
+		rows[i].textContent = array[i];
 	}
 }
-
-function validateSelection(item) {
-	return ITEMS.includes(item)
-}
-
 
 function computerPlay() {
 	let computerItem = ITEMS[Math.floor(Math.random() * ITEMS.length)];
 	return computerItem;
 }
 
+const btns = Array.from(document.querySelectorAll('button'));
 
-game();
+btns.forEach(element => {
+	element.addEventListener('click', choosePlayer);
+});
+
+
+
+function choosePlayer(event) {
+	if (isFirstRound(currentRound)) document.getElementById('results-container').style.display = 'block';
+	event.stopImmediatePropagation();
+	const playerChoice = event.target.value;
+	playRound(playerChoice, computerPlay());
+}
+
+function isFirstRound(round) {
+	if (round === 0) {
+		return true;
+	}
+	return;
+}
+
+window.onload = () => {
+	const audio = document.querySelector('audio');
+	console.log(`success ${audio}`);
+	audio.play();
+};
+// game();
